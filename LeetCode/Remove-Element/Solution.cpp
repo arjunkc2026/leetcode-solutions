d3d6b7d411602fcc07f1698ed327b93cedf02c73
arjunkc2026 <arjunkc2026@gmail.com>
2026-01-11 12:13:48 +0545

    Add solution for Remove Element

---
+++ LeetCode/Remove-Element/Solution.cpp
@@ -0,0 +1,14 @@
+1class Solution {
+2public:
+3    int removeElement(vector<int>& nums, int val) {
+4        int k = 0;  // index for elements != val
+5
+6        for (int i = 0; i < nums.size(); i++) {
+7            if (nums[i] != val) {
+8                nums[k] = nums[i];
+9                k++;
+10            }
+11        }
+12        return k;
+13    }
+14};
